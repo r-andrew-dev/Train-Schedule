f2e581b5ecdd68cd5bb31a5cc0878af8ccf13e8e
Rachael Andrew <r.andrew.dev@gmail.com>
2020-01-23 00:10:42 -0600

    tested and confirmed train information adding to database, clearing form afterwards, train added piece functioning properly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 
 // web app's Firebase configuration
-  var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyCJADMYgWX5sj_Bc2KAo8Lxd1JBHxLRTfM",
     authDomain: "train-scheduler-a8f1e.firebaseapp.com",
     databaseURL: "https://train-scheduler-a8f1e.firebaseio.com",
@@ -12,5 +12,49 @@
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 
-var db = firebase.database()
+const db = firebase.database()
+
+function removeText() {
+    $("#train-added").text("");
+}
+
+
+// submit button for adding train info.
+$("#submit").on("click", function(event) {
+    event.preventDefault();
+
+    // grabbing user inputs 
+    const trainName = $("#train-input").val().trim();
+    const destination = $("#destination-input").val().trim();
+    const firstTrainTime = $("#train-time-input").val().trim();
+    const frequency = $("#frequency-input").val().trim();
+
+    // creating temporary local object for holding data to push to firebase
+    const newTrain = {
+        train: trainName,
+        destination: destination,
+        time: firstTrainTime,
+        frequency: frequency, 
+    };
+
+    // pushes object to database.
+    db.ref().push(newTrain);
+    // updates text next to submit button
+    $("#train-added").text("Train added!");
+    // sets a function to remove text after five seconds.
+    setTimeout(removeText, 5000);
+
+    // clears all the text boxes
+    $("#train-input").val("");
+    $("#destination-input").val("");
+    $("#train-time-input").val("");
+    $("#frequency-input").val("");
+
+});
+
+
+
+
+
+
 
