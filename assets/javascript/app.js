4215b949bafe75daded3d19da62c03db6de4e567
Rachael Andrew <r.andrew.dev@gmail.com>
2020-01-22 23:10:56 -0600

    built html layout with bootstrap

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCJADMYgWX5sj_Bc2KAo8Lxd1JBHxLRTfM",
+    authDomain: "train-scheduler-a8f1e.firebaseapp.com",
+    databaseURL: "https://train-scheduler-a8f1e.firebaseio.com",
+    projectId: "train-scheduler-a8f1e",
+    storageBucket: "train-scheduler-a8f1e.appspot.com",
+    messagingSenderId: "1074058844849",
+    appId: "1:1074058844849:web:9d23134db9fb5f17196090"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+
+
+
+
+
+
+</script>
